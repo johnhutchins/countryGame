1d3550a5af95e6545ceb62527e12c23ce79cdd4f
John Hutchins <johnwallshutchins@gmail.com>
2020-04-01 10:56:11 -0400

    add cors

--- build/api/apiGetRandomCountry.js
+++ build/api/apiGetRandomCountry.js
@@ -1,18 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const data_1 = require("../data");
-const CountrySummary_1 = require("../model/CountrySummary");
 const getRandomCountry = () => {
     const randIndex = Math.floor(Math.random() * data_1.DataStore.countries.length);
     let obj = {
         country: data_1.DataStore.countries[randIndex].country,
         city: data_1.DataStore.countries[randIndex].city
     };
-    if (obj.city == null) {
-        obj.city = 'N/A';
-    }
     return obj;
 };
 exports.apiGetRandomCountry = (req, res, next) => {
-    return res.json(new CountrySummary_1.CountrySummary(getRandomCountry()));
+    const country = getRandomCountry();
+    return res.json(country);
 };
--- build/server.js
+++ build/server.js
@@ -4,13 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const apiGetRandomCountry_1 = require("./api/apiGetRandomCountry");
 const app = express_1.default();
-//app.use(express.static('public'))
-// //this needs to be fixed....
-// app.get('/', (req, res, next) => {
-//     res.json({ 'status': "You didn't mean to come here..." })
-// })
+app.use(cors_1.default());
 app.get('/country', apiGetRandomCountry_1.apiGetRandomCountry);
 app.listen(process.env.PORT || 8091, () => {
     console.log("Server Started");
