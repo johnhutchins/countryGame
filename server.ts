8a6e814ba53b0ff79d093d9a4101f746dc1ad3d0
John Hutchins <johnwallshutchins@gmail.com>
2020-04-01 10:33:47 -0400

    update modules

--- server.ts
+++ server.ts
@@ -4,16 +4,7 @@ import { apiGetRandomCountry } from "./api/apiGetRandomCountry"
 import path from "path"
 const app = express()
 
-
-//app.use(express.static('public'))
-// //this needs to be fixed....
-// app.get('/', (req, res, next) => {
-//     res.json({ 'status': "You didn't mean to come here..." })
-// })
-
-app.use(cors({
-    origin: 'http://localhost:3000/'
-}))
+app.use(cors())
 
 app.get('/country', apiGetRandomCountry)
 
