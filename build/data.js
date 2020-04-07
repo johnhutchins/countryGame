49ff5fee5a7860446e047ad58a6ffc36238345f5
John Hutchins <johnwallshutchins@gmail.com>
2020-04-06 21:28:35 -0400

    add build back

---
+++ build/data.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const AllCountries_json_1 = __importDefault(require("./data/json/AllCountries.json"));
+class DataStore {
+}
+exports.DataStore = DataStore;
+DataStore.countries = AllCountries_json_1.default;
---
+++ build/model/CountrySummary.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class CountrySummary {
+    constructor(data) {
+        this.country = data.country;
+        this.city = data.city;
+    }
+}
+exports.CountrySummary = CountrySummary;
