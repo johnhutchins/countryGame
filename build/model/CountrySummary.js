9cca936a9edd4b81b05fcba4104bbcee7b360733
John Hutchins <johnwallshutchins@gmail.com>
2020-04-06 21:26:24 -0400

    work toward testing

--- build/model/CountrySummary.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CountrySummary {
-    constructor(data) {
-        this.country = data.country;
-        this.city = data.city;
-    }
-}
-exports.CountrySummary = CountrySummary;
